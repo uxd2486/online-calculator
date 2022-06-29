5245ea92d2d887530efd05c6e0984f0913480d98
Utkarsh Dayal <uxd2486@rit.edu>
2022-06-29 19:20:03 -0400

    Added functions for basic math operations - add, subtract, multiply, divide

---
+++ app.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0){
+        return NaN;
+    }
+    return num1 / num2;
+}
